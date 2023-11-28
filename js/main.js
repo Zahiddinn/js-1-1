0fe26e5ee92d88b1c06eff27d4c04e9d71907dd7
Zahid <Zahiddin9423@gmail.com>
2023-11-29 02:40:59 +0400

    search num

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const squareLength = document.querySelector("#square-length");
 const squareWidth = document.querySelector("#square-width");
 const container = document.querySelector(".container");
 const btn = document.querySelector(".btn");
-const resultSquare =document.querySelector('.result-square');
+const resultSquare = document.querySelector(".result-square");
+const divider = document.querySelector("#divider");
+const btnDivider = document.querySelector(".btnDivider");
 
 // squareLength.addEventListener("change", slValue);
 // squareWidth.addEventListener("change", swValue);
@@ -16,15 +18,59 @@ const resultSquare =document.querySelector('.result-square');
 //   console.log(sW);
 // }
 btn.addEventListener("click", () => {
-  const sL = squareLength.value;
+  const sL = +squareLength.value;
 
-  const sW = squareWidth.value;
+  const sW = +squareWidth.value;
+  console.log(sL);
 
-  const square = 'Плошадь: '+(sL * sW * 2);
-  const perimeterRes = 'Периметр: '+((sL*sL) + (sW*sW));
+  const square = "Площадь: " + sL * sW;
+  const perimeterRes = "Периметр: " + (sL + sL + (sW + sW));
   console.log(square);
-  document.querySelector('.result-square').textContent = square;
-  document.querySelector('.perimeter').textContent = perimeterRes;
+  console.log(perimeterRes);
+  document.querySelector(".result-square").textContent = square;
+  document.querySelector(".perimeter").textContent = perimeterRes;
 });
 
-// box = document.childElementCount('h3')
+// box = document.childElementCount('h3'
+function prime(n) {
+  let result = [1, n];
+  for (let i = 2; i < Math.pow(n, 0.5); i++) {
+    if (n % i == 0) {
+      result.push(i);
+      result.push(n / i);
+    }
+  }
+  return result.sort((a, b) => a - b);
+}
+
+btnDivider.addEventListener("click", () => {
+  const num = divider.value;
+  console.log(prime(num));
+  const resul = prime(num);
+  document.querySelector(".result-divider").textContent = resul;
+});
+
+function divisor() {
+  let firstNumber = document.querySelector("#first-number").value;
+  let secondNumber = document.querySelector("#second-number").value;
+
+  while (firstNumber != secondNumber) {
+    if (firstNumber > secondNumber) {
+      firstNumber = firstNumber - secondNumber;
+      console.log(firstNumber + "a");
+    } else {
+      secondNumber = secondNumber - firstNumber;
+      console.log(secondNumber + "b");
+    }
+  }
+  let gcd = firstNumber;
+ console.log(gcd);
+ return gcd;
+
+}
+ const commonDivisor = document.querySelector(".commonDivisor")
+
+ commonDivisor.addEventListener("click", () => {
+  divisor()
+   
+ })
